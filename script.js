ebe1e8636907b82a3bf5a3ca05e307e1806c6d27
Volodymyr <akinaru3@gmail.com>
2026-06-21 18:57:09 +0300

    adapt everything

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-// const openBtn = document.getElementById("openModalBtn");
-// const backdrop = document.getElementById("modalBackdrop");
-// const closeBtn = document.querySelector("#closeModalBackdrop");
+const openBtn = document.getElementById("openModalBtn");
+const backdrop = document.getElementById("modalBackdrop");
+const closeBtn = document.querySelector("#closeModalBackdrop");
 
 const openHeaderBtn = document.getElementById("openHeaderBtn");
 const headerMobileWindow = document.getElementById("headerWindow");
 const closeHeaderBtn = document.querySelector("#closeHeaderBtn");
 
-// openBtn.addEventListener("click", () => {
-//   backdrop.classList.add("is-open");
-// });
+openBtn.addEventListener("click", () => {
+  backdrop.classList.add("is-open");
+});
 
-// closeBtn.addEventListener("click", () => {
-//   backdrop.classList.remove("is-open");
-// });
+closeBtn.addEventListener("click", () => {
+  backdrop.classList.remove("is-open");
+});
 
 openHeaderBtn.addEventListener("click", () => {
   headerMobileWindow.classList.add("is-open");
